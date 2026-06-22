9f4635b017ae6e7e89e7de23c7cdc5b39b219834
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2026-06-22 23:56:59 +0200

    Update serienstream.js
    Fix getLatestUpdates()

--- javascript/anime/src/de/serienstream.js
+++ javascript/anime/src/de/serienstream.js
@@ -42,13 +42,22 @@ class DefaultExtension extends MProvider {
     }
     async getLatestUpdates(page) {
         const baseUrl = this.source.baseUrl;
-        const res = await this.client.get(`${baseUrl}/neu`);
-        const elements = new Document(res.body).select("div.seriesListContainer div");
+        const res = await this.client.get(`${baseUrl}/beliebte-serien`);
+        const elements = new Document(res.body).select("div.mb-5:nth-child(3) > div:nth-child(2) div");
         const list = [];
+        const seen = new Set();
         for (const element of elements) {
             const linkElement = element.selectFirst("a");
-            const name = element.selectFirst("h3").text;
-            const imageUrl = baseUrl + linkElement.selectFirst("img").attr("data-src");
+            const img = linkElement.selectFirst("img");
+            const name = img.attr("alt");
+            // Skip duplicates
+            if (seen.has(name)) continue;
+            seen.add(name);
+            let imageUrl = img.attr("data-src");
+            if (!imageUrl || imageUrl.trim() === "") {
+                imageUrl = img.attr("src");
+            }
+            imageUrl = baseUrl + imageUrl;
             const link = linkElement.attr("href");
             list.push({ name, imageUrl, link });
         }
